5a3ebe41bbe24afed513aad1978efc7f2d8416ee
FedeMontenegro <fedejulianmontenegro@gmail.com>
2023-05-11 12:56:46 -0300

    Primer commit. Desarrollo del componente Header iniciado

---
+++ src/components/header/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Styles from "./styles/navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <nav className={Styles.nav}>
+        <ul className={Styles.ul}>
+            <li className={Styles.list}>Gmail</li>
+            <li className={Styles.list}>Imágenes</li>
+            <li className={Styles.list}>Apps</li>
+            <li className={Styles.list}>User</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
