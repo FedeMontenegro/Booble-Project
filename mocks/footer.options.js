5ee43f4ede4c93d409debe8b9df1b16278242977
FedeMontenegro <fedejulianmontenegro@gmail.com>
2023-05-18 14:48:12 -0300

    Se creó el componente Footer.

---
+++ mocks/footer.options.js
@@ -0,0 +1,40 @@
+export const sectionOne = [
+    {
+        name: "Argentina",
+        onClick: () => ""
+    }
+]
+
+export const sectionTwo = [
+    {
+        name: "Sobre Google",
+        onClick: () => ""
+    },
+    {
+        name: "Publicidad",
+        onClick: () => ""
+    },
+    {
+        name: "Negocios",
+        onClick: () => ""
+    },
+    {
+        name: "Cómo funciona la Búsqueda",
+        onClick: () => ""
+    },
+]
+
+export const sectionThree = [
+    {
+        name: "Privacidad",
+        onClick: () => ""
+    },
+    {
+        name: "Condiciones",
+        onClick: () => ""
+    },
+    {
+        name: "Preferencias",
+        onClick: () => ""
+    },
+]
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {default as Header} from "./header/Header.jsx"
 export {default as Logo} from "./logo/Logo.jsx"
 export {default as Browser} from "./browser/Browser.jsx"
+export {default as Footer} from "./footer/Footer.jsx"
+export {default as Navbar} from "./navbar/Navbar.jsx"
